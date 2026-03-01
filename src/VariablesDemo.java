d596980a0de0befce56def3653cfa53846ff3f08
pratikshivale <pratikshivale07@gmail.com>
2026-03-02 02:13:12 +0530

    Added VariablesDemo program with different data types

---
+++ src/VariablesDemo.java
@@ -0,0 +1,17 @@
+// Program 2
+/*Description: Declare different types
+               Print them properly */
+
+
+public class VariablesDemo {
+    public static void main(String[] args){
+        double height = 15.5;
+        char firstLetter = 'A';
+        boolean isStudent = true;
+
+        System.out.println("Height = "+ height);
+        System.out.println("First letter = "+ firstLetter);
+        System.out.println("IsStudent = "+ isStudent);
+
+    }
+}
