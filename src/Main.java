b83c3a998638c38019540c76cd9abfa5013bc92d
pratikshivale <pratikshivale07@gmail.com>
2026-03-07 19:05:41 +0530

    Added operator demo and practice programs

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//Program 1
+//Description: Print by introduction
+
+class main{
+    public static void main(String[]args) {
+        System.out.println("Hello My self Pratik.");
+        System.out.println("Now i an start to do java journey");
+    }
+}
